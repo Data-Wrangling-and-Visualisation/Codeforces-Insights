b40c47871234f82d5e5e1c82279589b2124b3797
spiriton <vanya.ershov.05@mail.ru>
2025-04-20 01:36:58 +0300

    feat: add tasks rating distribution chart

--- frontend/src/pages/Tasks.js
+++ frontend/src/pages/Tasks.js
@@ -1,11 +1,13 @@
 // src/pages/UserRating.js
 import React from "react";
 import TasksSolvabilityChart from "../components/TasksSolvabilityChart";
+import TasksRatingDistributionChart from "../components/TasksRatingDistributionChart";
 
 const Tasks = () => {
   return (
     <div className="p-8">
         <TasksSolvabilityChart/>
+        <TasksRatingDistributionChart/>
     </div>
   );
 };
